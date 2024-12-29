885c5616a74cc89280b5645d9e7883f156d301c3
Ewik <ewikk77@gmail.com>
2024-12-29 14:51:11 +0100

    swagger added to both dev and prod

--- ECUBackend/ECUBackend/Program.cs
+++ ECUBackend/ECUBackend/Program.cs
@@ -14,20 +14,20 @@ builder.Services.AddSingleton<SensorDataService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+if (true)//app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
