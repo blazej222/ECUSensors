91e6133f1be3514583bc265c3f777df2fd413399
Ewik <ewikk77@gmail.com>
2024-12-29 14:01:43 +0100

    working project...

---
+++ ECUBackend/ECUBackend/Models/SensorData.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ECUBackend.Models
+{
+    public class SensorData
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; } // MongoDB ObjectId
+        public DateTime Timestamp { get; set; }
+        public string SensorType { get; set; }
+        public string Instance { get; set; }
+        public double Value { get; set; }
+
+    }
+}
