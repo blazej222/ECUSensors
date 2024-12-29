0d0c6751c7eb58cc5dbdbf7830d2364e6525095b
Ewik <ewikk77@gmail.com>
2024-12-29 22:00:51 +0100

    basic sensors api requests

--- ECUBackend/ECUBackend/Controllers/SensorsController.cs
+++ ECUBackend/ECUBackend/Controllers/SensorsController.cs
@@ -5,7 +5,8 @@ using ECUBackend.Models;
 namespace ECU.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/sensors")]
     public class SensorsController : ControllerBase
     {
         private readonly SensorDataService _sensorDataService;
@@ -14,38 +15,38 @@ namespace ECU.Controllers
             _sensorDataService = sensorDataService;
 
         [HttpGet]
-        public async Task<List<SensorData>> Get() =>
-            await _sensorDataService.GetAsync();
+        public async Task<List<string>> GetSensors() =>
+            await _sensorDataService.GetDistinctSensors();
 
-        [HttpGet("filter")]
+        [HttpGet("data")]
+        public async Task<List<SensorData>> GetAllData() =>
+            await _sensorDataService.GetAll();
+
+        [HttpGet("types")]
+        public async Task<List<string>> GetTypes() =>
+            await _sensorDataService.GetDistinctSensorTypes();
+
+
+        [HttpGet("data/filter")]
         public async Task<ActionResult<List<SensorData>>> GetFiltered(
             [FromQuery] string sensorType,
+            [FromQuery] uint? instanceId,
             [FromQuery] DateTime? startDate,
-            [FromQuery] DateTime? endDate)
+            [FromQuery] DateTime? endDate,
+            [FromQuery] string? sortBy,
+            [FromQuery] bool? ascending,
+            [FromQuery] int? limit
+            )
         {
-            var data = await _sensorDataService.GetSensorData(sensorType, startDate, endDate);
+            var data = await _sensorDataService.GetSensorData(sensorType, instanceId, startDate, endDate, sortBy, ascending, limit);
             return Ok(data);
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Post([FromBody] SensorData data)
-        {
-            await _sensorDataService.InsertSensorData(data);
-            return CreatedAtAction(nameof(Get), new { id = data.Id }, data);
-        }
-
-        [HttpGet("check-connection")]
-        public async Task<ActionResult> CheckConnection()
-        {
-            bool isConnected = await _sensorDataService.CheckMongoConnection();
-            if (isConnected)
-            {
-                return Ok("MongoDB connection successful");
-            }
-            else
-            {
-                return StatusCode(500, "MongoDB connection failed");
-            }
-        }
+        //[HttpPost]
+        //public async Task<ActionResult> Post([FromBody] SensorData data)
+        //{
+        //    await _sensorDataService.InsertSensorData(data);
+        //    return CreatedAtAction(nameof(Get), new { id = data.Id }, data);
+        //}
     }
 }
--- ECUBackend/ECUBackend/Services/SensorDataService.cs
+++ ECUBackend/ECUBackend/Services/SensorDataService.cs
@@ -1,6 +1,7 @@
 ﻿using ECUBackend.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using System.Runtime.CompilerServices;
 
 namespace ECUBackend.Services
 {
@@ -19,19 +20,86 @@ namespace ECUBackend.Services
         public async Task InsertSensorData(SensorData data) =>
             await _sensorDataCollection.InsertOneAsync(data);
 
-        public async Task<List<SensorData>> GetAsync() =>
+        public async Task<List<SensorData>> GetAll() =>
             await _sensorDataCollection.Find(_ => true).ToListAsync();
 
         public async Task<List<SensorData>> GetSensorData(
-            string sensorType, DateTime? startDate, DateTime? endDate) =>
-            await _sensorDataCollection.Find(x =>
-                (string.IsNullOrEmpty(sensorType) || x.SensorType == sensorType) &&
-                (!startDate.HasValue || x.Timestamp >= startDate) &&
-                (!endDate.HasValue || x.Timestamp <= endDate))
+            string sensorType, uint? instanceId = null, DateTime? startDate = null, DateTime? endDate = null,
+            string sortBy = null, bool? ascending = null, int? limit = 0)
+        {
+            //await _sensorDataCollection.Find(x =>
+            //    (string.IsNullOrEmpty(sensorType) || x.SensorType == sensorType) &&
+            //    (!instanceId.HasValue || x.InstanceId == instanceId) &&
+            //    (!startDate.HasValue || x.Timestamp >= startDate) &&
+            //    (!endDate.HasValue || x.Timestamp <= endDate))
+            //    .ToListAsync();
+
+            var filter = Builders<SensorData>.Filter.And(
+                (string.IsNullOrEmpty(sensorType) ? Builders<SensorData>.Filter.Empty : Builders<SensorData>.Filter.Eq(x => x.SensorType, sensorType)),
+                (!instanceId.HasValue ? Builders<SensorData>.Filter.Empty : Builders<SensorData>.Filter.Eq(x => x.InstanceId, instanceId)),
+                (!startDate.HasValue ? Builders<SensorData>.Filter.Empty : Builders<SensorData>.Filter.Gte(x => x.Timestamp, startDate)),
+                (!endDate.HasValue ? Builders<SensorData>.Filter.Empty : Builders<SensorData>.Filter.Lte(x => x.Timestamp, endDate))
+            );
+
+            if (string.IsNullOrEmpty(sortBy) && ascending.HasValue)
+            {
+                return await _sensorDataCollection
+                .Find(filter)
+                .Limit(limit)
+                .ToListAsync();
+            }
+            if (string.IsNullOrEmpty(sortBy)) sortBy = "Timestamp";
+
+            // ASP.NET serializes property names to camelCase by default in JSON
+            if (sortBy.Length > 0 && char.IsLower(sortBy[0])) sortBy = char.ToUpper(sortBy[0]) + sortBy.Substring(1);
+            var sort = ascending.HasValue && ascending.Value
+            ? Builders<SensorData>.Sort.Ascending(sortBy)
+            : Builders<SensorData>.Sort.Descending(sortBy);
+
+            return await _sensorDataCollection
+                .Find(filter)
+                .Sort(sort)
+                .Limit(limit)
+                .ToListAsync();
+        }
+
+        public async Task<bool> DeleteSensorDataAsync(FilterDefinition<SensorData> filter)
+        {
+            try
+            {
+                var deleteResult = await _sensorDataCollection.DeleteManyAsync(filter);
+                return deleteResult.DeletedCount > 0;
+            }
+            catch (Exception ex)
+            {
+                // Log exception as needed
+                Console.WriteLine($"Error deleting sensor data: {ex.Message}");
+                return false;
+            }
+        }
+
+        public async Task<List<string>> GetDistinctSensorTypes()
+        {
+            var distinctSensorTypes = await _sensorDataCollection
+                .Aggregate()
+                .Group(x => x.SensorType, g => new { SensorType = g.Key })
+                .Project(result => result.SensorType)
                 .ToListAsync();
 
-        public async Task<List<SensorData>> GetAll() =>
-            await _sensorDataCollection.Find(_ => true).ToListAsync();
+            return distinctSensorTypes;
+        }
+
+        public async Task<List<string>> GetDistinctSensors()
+        {
+            var distinctSensorTypes = await _sensorDataCollection
+                .Aggregate()
+                .Group(x => new { x.SensorType, x.InstanceId }, g => new { g.Key.SensorType, g.Key.InstanceId })
+                .Project(result => result.SensorType + result.InstanceId)
+                .ToListAsync();
+
+            distinctSensorTypes.Sort();
+            return distinctSensorTypes;
+        }
 
         public async Task<bool> CheckMongoConnection()
         {
