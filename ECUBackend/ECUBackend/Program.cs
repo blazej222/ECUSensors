b173b53a4b5f8df4a679ccf45e8a1e43e0de32ae
KaraitPL <karaitpl@gmail.com>
2025-01-01 18:27:48 +0100

    CORS error fixed

--- ECUBackend/ECUBackend/Program.cs
+++ ECUBackend/ECUBackend/Program.cs
@@ -16,8 +16,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") // Adres frontendu
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
+
+app.UseCors("AllowFrontend");
 
 // Configure the HTTP request pipeline.
 if (true)//app.Environment.IsDevelopment())
